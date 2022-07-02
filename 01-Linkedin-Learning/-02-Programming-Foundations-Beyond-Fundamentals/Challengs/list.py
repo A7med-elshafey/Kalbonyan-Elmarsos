625cec269fa28d0afb9801ab840342dc0a3bf01f
A7med-elshafey <101415932+A7med-elshafey@users.noreply.github.com>
2022-07-02 12:10:03 +0200

    Done

---
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challengs/list.py
@@ -0,0 +1,13 @@
+
+
+flips = [
+    'heads',
+    'tails',
+    'tails',
+    'heads',
+    'tails',
+]
+# to print the count of tails no heads it's ok .
+print(flips.count('tails'))
+# and this pop also print tails :)
+print(flips.pop())
